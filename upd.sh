353824407b1a9bd15ba4d0febad6c9dcc18c4234
Nathan Rossin <208092820+nrossin@users.noreply.github.com>
2025-11-19 11:07:55 -0500

    Updated jar

--- upd.sh
+++ upd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ../specfile_explorer/target/specfile_explorer-1.0.1_SNAPSHOT.jar ./specfile_explorer.jar
+cp ../specfile_explorer/target/specfile_explorer-0.1.0-SNAPSHOT.jar ./specfile_explorer.jar
 
 git add .
 git commit -m 'Updated jar'
